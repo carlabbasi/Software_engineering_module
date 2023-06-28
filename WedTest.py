58877faf4bff38169f56cd2e192ae299bc48ba4e
carlabbasi <135010074+carlabbasi@users.noreply.github.com>
2023-06-28 12:39:30 +0100

    Update WedTest.py

--- WedTest.py
+++ WedTest.py
@@ -1,7 +1,7 @@
 #WedTest
-print("#2.1.1.19 LAB")
 
-print("#Original arrow")
+#2.1.1.19 LAB
+#Original arrow
 print("    *")
 print("   * *")
 print("  *   *")
@@ -11,24 +11,25 @@ print("  *   *")
 print("  *   *")
 print("  *****", end="\n\n")
 
-print("#Arrow with less print invocations (2)")
+#Arrow with less print invocations (2)
 print("    *", "\n", "   * *", "\n", "  *   *", "\n", " *     *", "\n", "***   ***", "\n" , sep="", end="")
 print("  *   *", "\n", "  *   *", "\n", "  *****", sep="", end="\n\n")
 
-print("#Double size arrow")
+#Double size arrow"
 print("        *", "\n", "       * *", "\n", "      *   *", "\n", "     *     *", "\n", sep="", end="")
 print("    *       *", "\n", "   *         *", "\n","  *           *", "\n", sep="", end="")
 print(" *             *", "\n", "******     ******", "\n", "     *     *", "\n","     *     *", "\n", sep="", end="")
 print("     *     *", "\n", "     *     *", "\n", "     *******", sep="", end="\n\n")
 
-print("#2x Arrows")
+#2x Arrows"
 print("    *    " *2, "\n", "   * *   " *2, "\n", "  *   *  "*2, "\n", " *     * " *2, "\n", sep="", end="")
 print("***   ***" * 2, "\n" ,"  *   *  "*2, "\n", "  *   *  " *2, "\n", "  *****  " *2, sep="", end="\n\n")
 
-print("2.2.1.11 LAB")
+
+#"2.2.1.11 LAB"
 print('"I\'m"', "\n", "\"\"learning\"\"", "\n", '"""Python"""', end="\n\n")
 
-print("2.2.1.11 SUMMARY")
+#2.2.1.11 SUMMARY
 print("#Ex1 = Str, Str")
 print("#Ex2 = Str, Float, Int, Bool")
 print("#Ex3 = 11")
