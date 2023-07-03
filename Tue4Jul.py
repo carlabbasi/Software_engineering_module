0dc316453507bdc37326f8c8ed66965d712d43ed
carlabbasi <135010074+carlabbasi@users.noreply.github.com>
2023-07-03 18:06:25 +0100

    Update Tue4Jul.py

--- Tue4Jul.py
+++ Tue4Jul.py
@@ -1,4 +1,4 @@
-# Tue4Jul.py
+# Tue Test
 
 # 3.1.1.12 LAB - Leap year checker
 # input
@@ -23,21 +23,25 @@ print("\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n")
 # 3.2.1.3 LAB - garry trotter secret number guesser
 import time as snooze                                           # import time module as snooze
 
-print(
-"""
-+================================+     ^               
-| Welcome to my game, muggle!    |    /_\\
-| Enter an integer number        |     O  |
-| and guess what number I've     |   --|--|
-| picked for you.                |     |  |
-| So, what is the secret number? |    / \\
-+================================+   /   \\
-""")                                                            # welcome message
+print("+================================+     ^")               # welcome message
+snooze.sleep(0.5)
+print("| Welcome to my game, muggle!    |    /_\\")
+snooze.sleep(0.5)
+print("| Enter an integer number        |     O  |")
+snooze.sleep(0.5)
+print("| and guess what number I've     |   --|--|")
+snooze.sleep(0.5)
+print("| picked for you.                |     |  |")
+snooze.sleep(0.5)
+print("| So, what is the secret number? |    / \\")
+snooze.sleep(0.5)
+print("+================================+   /   \\")
+snooze.sleep(0.5)
 
 secret_number = 777                                             # declare secret number
 guesses = 1                                                     # total guesses variable
-guess_lst = []                                                  # empty list of guesses
-guess = int(input("Enter your guess: "))                        # number guess prompt
+guess_lst = []                                                  # empty list to catch previous guesses
+guess = int(input("\nEnter your guess: "))                      # number guess prompt
 
 while guess != 777:                                             # while guess is not secret number enter loop
     print("Hmmm,thinking......")                                # calculating guess
@@ -46,15 +50,15 @@ while guess != 777:                                             # while guess is
     guesses += 1                                                # increment guesses by 1 after each guess
     guess_lst.append(guess)                                     # append last guess to guess list
 
-    if guess == 7:                                              # if guess is 7 say that's the right first number
+    if guess == 7:                                              # if == 7 say that's the right first number
         print("That's the correct first number! Keep guessing")
         guess = int(input("Enter your guess: "))
 
-    elif guess == 77:                                           # if guess is 77 say that's the right first two numbers
+    elif guess == 77:                                           # if == 77 say that's the right first two numbers
         print("That's the correct first two numbers! Keep guessing")
         guess = int(input("Enter your guess: "))
 
-    elif "7" in str(guess):                                      # if "7" appears in guess, say a number is correct
+    elif "7" in str(guess):                                      # if "7" appears in guess at all, a number is correct
         print("At least one of those numbers is correct!")
         guess = int(input("Enter your guess: "))
 
@@ -79,12 +83,11 @@ while user != "Y":
     user = input("Ready to count now? (Y/N): ")
     user = user.upper()
 
-for count in range(1, 6):                           # for loop, ranging through 1-5
+for count in range(1, 6):                           # for loop from 1-5
         dot = "."                                   # define dot as "."
         dots = count * dot                          # for each iteration the count increases the amount of "."
-        print(f"{count} Mississippi{dots}")         # output to screen count increments with increasing number of dots
+        print(f"{count} Mississippi{dots}")         # output to screen count increments with same number of dots
         time.sleep(1)                               # sleep for 1 second
 
 print("\nReady or not, here I come!")               # on completion of loop print statement
 print("\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n")
-
