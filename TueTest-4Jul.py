e817214d0caa126a4a71eab1a3c9e3ebb8d7b5f6
carlabbasi <135010074+carlabbasi@users.noreply.github.com>
2023-07-03 12:00:35 +0100

    Update TueTest-4Jul.py

--- TueTest-4Jul.py
+++ TueTest-4Jul.py
@@ -1,1 +1,18 @@
+# 3.1.1.12 Year calculator
+year = int(input("Enter a year: "))                     # enter year
 
+if year < 1582:                                         # year before 1582?
+    print("Year is before the start of the Gregorian calendar")
+
+elif year % 4 != 0:                                     # year divisible by 4?
+    print(f"{year} is a common year.")
+
+elif year % 100 !=0:                                    # year divisible by 100?
+    print(f"{year} is a leap year.")
+
+elif year % 400 !=0:
+    print(f"{year} is a common year.")                  # year divisible by 400?
+
+else:
+    print(f"{year} is a leap year.")                    # else = leap year
+print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n")
