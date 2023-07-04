b278763a99053182eff902ae3a9862fcc58e6365
carlabbasi <135010074+carlabbasi@users.noreply.github.com>
2023-07-04 13:16:40 +0100

    Create Wed5Jul.py

---
+++ Wed5Jul.py
@@ -0,0 +1,13 @@
+# Wed5July.py
+
+# 3.2.1.9 LAB
+print("While loop secret word guesser")
+
+user = ""                                                      # set variable to empty string
+
+while True:                                                    # enter while loop
+  if user == "chupacabra":                                     # if / break clause
+    break
+  user = input("Guess the secret word. What is your guess: "?  # user input
+
+print("You've successfully left the loop")                     # loop exit statement
