597e88186c137c41c115ae7dc22e9b34da5e4f36
carlabbasi <135010074+carlabbasi@users.noreply.github.com>
2023-07-05 16:31:01 +0100

    Update Fri7Jul.py

--- Fri7Jul.py
+++ Fri7Jul.py
@@ -2,52 +2,55 @@
 
 # 3.4.1.6 LAB - Basics of lists
 
-hat_list = [1, 2, 3, 4, 5]
+hat_list = [1, 2, 3, 4, 5]                      # crate list and add values
 
-user_input = (int(input("Enter an integer number: ")))
+user_input = (int(input("Enter an integer number: ")))  # prompt user to add value
 
-hat_list[2] = user_input
+hat_list[2] = user_input                        # replace list index [2] with user input
 
-del hat_list[-1]
+del hat_list[-1]                                # delete last item in list
 
-print(len(hat_list))
+print(len(hat_list))                            # print length of hat list
 
-print(hat_list)
+print(hat_list)                                 # print list
 
 
 # 3.4.1.13 LAB - the BEATLES
 
-beatles = []
+beatles = []                                    # create empty list
 print("Step 1:", beatles)
 
-beatles.append("John Lennon")
-beatles.append("PaulMcCartney")
-beatles.append("George Harrison")
+beatles.append("John Lennon")                   # append name
+beatles.append("PaulMcCartney")                 # append name
+beatles.append("George Harrison")               # append name
 print("Step 2:", beatles)
 
-for names in range(2):
-    if "Stu Sutcliffe" not in beatles:
-        input1 = input("Type \"Stu Sutcliffe\" to add to beatles list: ")
-        if input1 != "Stu Sutcliffe":
-            continue
-        else:
-            beatles.append(input1)
-
-    if "Pete Best" not in beatles:
-        input2 = input("Type \"Pete Best\" to add to beatles list: ")
-        if input2 != "Pete Best":
-            continue
-        else:
-            beatles.append(input2)
+for names in range(1):                          # for loop to add 2 names to list
+    while True:
+        if "Stu Sutcliffe" not in beatles:      # while loop to make sure name spelt correctly
+            input1 = input("Type \"Stu Sutcliffe\" to add to beatles list: ")
+            if input1 != "Stu Sutcliffe":
+                continue
+            else:
+                beatles.append(input1)          # if input correct append to list
+                break
+    while True:
+        if "Pete Best" not in beatles:          # while loop to make sure name spelt correctly
+            input2 = input("Type \"Pete Best\" to add to beatles list: ")
+            if input2 != "Pete Best":
+                continue
+            else:
+                beatles.append(input2)          # if input correct append to list
+                break
+    break                                       # break for loop
 print("Step 3:", beatles)
 
-del(beatles[-1])
-del(beatles[-1])
+for name in range(2):                           # for loops to delete last list entry (repeats twice)
+    del(beatles[-1])
 print("Step 4:", beatles)
 
-beatles.insert(0, "Ringo Star")
+beatles.insert(0, "Ringo Star")                 # insert into list at index 0 (first)
 print("Step 5:", beatles)
 
-
-# testing list legth
+# testing list legth                            # print list length
 print("The Fab", len(beatles))
