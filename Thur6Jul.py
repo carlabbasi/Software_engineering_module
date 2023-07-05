143080ad37f4299e5401c302fbc79ba8f25d64d6
carlabbasi <135010074+carlabbasi@users.noreply.github.com>
2023-07-05 21:15:07 +0100

    Update Thur6Jul.py

--- Thur6Jul.py
+++ Thur6Jul.py
@@ -53,12 +53,12 @@ if flag_register & mask:
 else:
     print("Windmill is off")
 
-# The above should output (ingoring the # of course)
-# bit is set
-# bit is reset
-# bit is set
-# bit is reset
-# bit is set
+# The above should output:
+# bit set
+# bit reset
+# bit set
+# bit reset
+# bit set
 print("\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n")
 
 
@@ -94,7 +94,7 @@ while user == "Y":
         print("\nCalculator only works with positive integer numbers (no strings)")
         continue                    # continue loop if error detected
 
-    except KeyboardInterrupt:       # error handling for keyboard interrupt
+    except KeyboardInterrupt:                           # error handling for keyboard interrupt
         user = "N"
         continue
 
@@ -117,7 +117,7 @@ while user == "Y":
         if blocks < counter:        # counter will always count blocks required for the next full layer
             break                   # if blocks is lower than counter, loop breaks
 
-    # when loop is broken, prints how many blocks were initially entered from the 'total_blocks' variable, how many-
+    # when loop is broken, prints how many blocks were initially entered from the 'total_blocks' variable, how many
     # full layers were achieved in the 'height' variable and how many blocks are remaining in the 'blocks' variable
     print(f"""
     With {total_blocks} blocks you can build a pyramid with {height} layer(s).
@@ -130,7 +130,7 @@ while user == "Y":
         user = "N"
         continue
 
-print("\n*** Application terminated ***")   # application terminates if user inputs anything other than 'Y'.
+print("\n*** Application terminated ***")           # application terminates if user inputs anything other than 'Y'.
 print("\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n")
 
 
@@ -140,28 +140,22 @@ print("*** Collatz's hypothesis ***")
 user = "Y"                                  # set user variable to Y for application control
 while user == "Y":                          # while loop for application control
     c0_numbers = []                         # create empty list to store each number that c0 becomes during calculation
-
     try:                                    # create exception to avoid value error if a string is input
         c0 = int(input("\nEnter a value to run against Collatz's hypothesis: "))        # user enters a value
-
     except ValueError:
         print("\nValue must be non-negative non-zero integer number!")  # message if Value Error detected
         continue
-
     except KeyboardInterrupt:               # error handling for keyboard interrupt
         user = "N"
         continue
-
     if c0 == 0:                             # if statement to check if c0 is == 0
         print("\nValue must be non-zero integer number!")
         continue
-
     if "-" in str(c0):                      # if statement to check if c0 is a negative number
         print("\nValue must be non-negative integer number!")
         continue
 
     steps = 0                               # steps variable to count total amount of steps to get c0 to a value of 1
-
     while c0 != 1:                          # while loop which stops when c0 == 1
         if c0 % 2 == 0:                     # check if c0 is even
             c0 /= 2                         # if even divide c0 by 2
@@ -173,8 +167,8 @@ while user == "Y":                          # while loop for application control
             steps += 1                      # steps count incremented
             c0_numbers.append(int(c0))      # append current c0 value to c0_numbers list
 
-    print(f"\n{c0_numbers}")                # once c0 == 1, print c0_numbers list containing all previous c0 values
-    print(f"Total steps taken = {steps}")   # print steps variable containing count of steps taken to get to 1
+    print(f"\n{c0_numbers}")      # once c0 == 1, print c0_numbers list containing all previous c0 values
+    print(f"Total steps taken to reach 1 = {steps}")   # print count of steps taken to get to 1
 
     try:                                    # ask if the user wants to try another number
         user = input("\nPress Y to try another number, anything else to quit: ")  # if Y inputted, application control-
