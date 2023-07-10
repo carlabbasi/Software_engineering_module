4b5683ccaf1fd4d19caff7b34e02f9ed74c9d100
carlabbasi <135010074+carlabbasi@users.noreply.github.com>
2023-07-10 08:17:52 +0100

    Update Mon10Jul.py

--- Mon10Jul.py
+++ Mon10Jul.py
@@ -5,7 +5,7 @@
 my_list = [1, 2, 4, 4, 1, 4, 2, 6, 2, 9]        # list
 temp_list = []                                  # temp list to store unique numbers
 
-for number in my_list[:]:                       # for loop to iterate through numbers in my_list
+for number in my_list:                       # for loop to iterate through numbers in my_list
     if number not in temp_list:                 # if number is not in the new list:
         temp_list.append(number)                # append the number to new_list
     else:
